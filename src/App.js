4e06b17f750a06183b1199be2e24ca1e701a8c90
Viswak23 <Viswak.RameshBalakrishnan@metropolia.fi>
2024-09-23 14:28:49 +0530

    completed activity 2

---
+++ src/App.js
@@ -0,0 +1,15 @@
+// App.js
+import SingleCounter from "./SingleCounter"; // Import the SingleCounter component
+import "./App.css"; // Import styles for the app
+
+const App = () => {
+  return (
+    <div className="app-container">
+      <SingleCounter />
+      <SingleCounter />
+      <SingleCounter />
+    </div>
+  );
+};
+
+export default App;
